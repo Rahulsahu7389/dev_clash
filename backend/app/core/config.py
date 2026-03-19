69b0b11236c208a78cda0017dfd7c503726d4cc9
Ashutosh Behera <ashupoco66@gmail.com>
2026-03-19 17:57:36 +0530

    changes in config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -5,12 +5,13 @@ from pydantic_settings import BaseSettings
 load_dotenv()
 
 class Settings(BaseSettings):
-    MONGO_URI: str = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+    MONGO_URI: str = os.getenv("MONGO_URI", "")
     JWT_SECRET_KEY: str = os.getenv("JWT_SECRET_KEY", "your_secret_key")
     ALGORITHM: str = os.getenv("ALGORITHM", "HS256")
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     DATABASE_NAME: str = "dev_clash_db"
     GEMINI_API_KEY: str = os.getenv("GEMINI_API_KEY", "")
+    YOUTUBE_API_KEY: str = os.getenv("YOUTUBE_API_KEY","")
 
     class Config:
         env_file = ".env"
